3b02d451e651b282a39d18c43da29852fc4d4e4b
pbhatgit <pari1608@gmail.com>
2020-08-10 23:15:39 +0200

    File modified v2

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,12 @@
 public class Welcome{
-	Public static void main(String[] args){}
+	public static void main(String[] args){
 	System.out.println("Welcome to DevOps.....");
-	System.out.println("Modified....Welcome to DevOps.....");
+	
+	
+	int a = 2;
+	int b = 18;
+	int s = a+b;
+	System.out.println("Sum is "+s);
 
 }
 }
